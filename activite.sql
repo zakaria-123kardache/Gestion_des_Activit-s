79a2eb3ee3e8c7a38338008c1a73c3400de051b9
unknown <zz240614@gmail.com>
2024-12-11 17:20:55 +0100

    add yml

--- activite.sql
+++ activite.sql
@@ -1,14 +1,19 @@
+CREATE DATABASE GestionBDE;
+
+USE GestionBDE;
+
+
 CREATE TABLE BDE (
     id_bde INT PRIMARY KEY NOT NULL,
     nom VARCHAR (30),
     prenom VARCHAR (30),
     email VARCHAR (15),
-    rôle  VARCHAR (15),
+    rank  ENUM ('president', 'tresorier','secretaire','membre_actif')DEFAULT 'membre_actif',
     date_ahesion DATE,
      
 );
 CREATE TABLE evenement (
-    id_event INT PRIMARY KEY NOT NULL,
+    id INT PRIMARY KEY NOT NULL,
     nom VARCHAR (30),
     description_event VARCHAR (30),
     date_heurs DATE,
@@ -18,19 +23,43 @@ CREATE TABLE evenement (
      
 );
 CREATE TABLE sponsors (
-    id_sponsor INT PRIMARY KEY NOT NULL,
+    id INT PRIMARY KEY NOT NULL,
     nom_de_entreprise VARCHAR (30),
     entite VARCHAR (30),
-    montant_sponsorise VARCHAR (15),
+    montant VARCHAR (15),
     telephone  VARCHAR (15),
     email VARCHAR(20),
      
 );
 CREATE TABLE participant (
-    id_participant INT PRIMARY KEY NOT NULL,
+    id INT PRIMARY KEY NOT NULL,
     nom VARCHAR (30),
     prenom VARCHAR (30),
     email VARCHAR (15),
+    rank  ENUM ('etudiant actif', 'etudiant diplome','etudiant invite')DEFAULT 'etudiant actif',
     statut VARCHAR(20),
+     
+);
+CREATE TABLE financement (
+    PRIMARY KEY id,
+    PRIMARY KEY id,
+   FOREIGN KEY (id_event) REFERENCES evenement (id), 
+   FOREIGN KEY (id_sponsor) REFERENCES sponsors (id), 
+     
+);
+CREATE TABLE animation (
+    PRIMARY KEY id,
+    PRIMARY KEY id,
+   FOREIGN KEY (id_event) REFERENCES evenement (id), 
+   FOREIGN KEY (id_bde) REFERENCES BDE (id),  
+     
+);
+CREATE TABLE inscrire (
+    PRIMARY KEY id,
+    PRIMARY KEY id,
+    PRIMARY KEY id,
+   FOREIGN KEY (id_participant) REFERENCES participant (id), 
+   FOREIGN KEY (id_event) REFERENCES événement (id), 
+   FOREIGN KEY (id_bde) REFERENCES BDE (id), 
      
 );
